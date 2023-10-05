c9a6e5ae44ba95f43bd691c161a6257904dfae8a
Ahmed Mohamed Elshalakany <shalakanyam@gmail.com>
2023-10-06 01:26:29 +0300

    Update 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-  add = magic_calculation_102.add
-  sub = magic_calculation_102.sub
-  if a < b:
-    c = add(a, b)
-  else:
-    c = sub(a, b)
-  for i in range(4, 6):
-    c = add(c, i)
-  return c
+    # import add and sub from magic_calculation_102 module
+    from magic_calculation_102 import add, sub
+    # if a is less than b, do the following
+    if a < b:
+        # initialize c as the sum of a and b
+        c = add(a, b)
+        # loop from 4 to 6 (excluding 6)
+        for i in range(4, 6):
+            # add i to c and update c
+            c = add(c, i)
+        # return c
+        return c
+    # otherwise, do the following
+    else:
+        # return the difference of a and b
+        return sub(a, b)
