90d536fca1503e035abc5ab35ecb245a3727f29b
Moshalakany <shalakanyam@gmail.com>
2024-02-13 17:28:09 +0200

    completed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
-} else{
-  console.log(argv[2]);	
+} else {
+  console.log(process.argv[2]);
 }
